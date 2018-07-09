f3bc7a44793f091ef91c64f64795f1ad69f57d64
Andrei Vasilev <amlk158@gmail.com>
2018-07-09 10:41:34 +0300

    vk auth fixed

--- config/passport-vk.js
+++ config/passport-vk.js
@@ -10,26 +10,45 @@ module.exports = (passport, configAuth) => {
     clientID:     configAuth.vk.clientID, // VK.com docs call it 'API ID', 'app_id', 'api_id', 'client_id' or 'apiId'
     clientSecret: configAuth.vk.clientSecret,
     callbackURL:  configAuth.vk.callbackURL,
+    passReqToCallback: true, // allows us to pass in the req from our route (lets us check if a user is logged in or not)
     scope: ['email'],
     profileFields: ['email'],
   },
-  function (accessToken, refreshToken, params, profile, done) {
-    console.log(accessToken)
-    console.log(refreshToken)
-    console.log(params)
-    console.log(profile)
-
-
+  function (req, accessToken, refreshToken, params, profile, done) {
+    console.log(profile, params)
+    if(!req.user) {
+      User.findOne({'vk.id': profile.id}).then(user => {
+        if(user) {
+          if(!user.vk.token) {
+            user.vk.token = accessToken
+            user.save().then(user => done(null, user))
+          }
+          return done(null, user)
+        } else {
           const newUser = new User({
             vk: {
               id: profile.id,
-              token: accessToken,
               name: profile.displayName,
-              avatar: profile.photos[0].value
+              token: accessToken,
+              avatar: profile.photos[0].value,
+              profileURL: profile.profileUrl
             }
-          }).save()
-            .then(newUser => done(null, newUser));
+          })
+          .save()
+          .then(newUser => done(null, newUser))
         }
+      })
+    } else {
+      let user = req.user
 
+      user.vk.id = profile.id
+      user.vk.name = profile.displayName
+      user.vk.token = accessToken
+      user.vk.avatar = profile.photos[0].value
+      user.save().then(user => done(null, user))
+
+    }
+  }
 ));
+
 }
